d5829fb9e533ea071e4a7dfe01495a376a4bd3a8
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-01-28 19:28:12 +0100

    Remove hide/show of buttons

--- src/events.js
+++ src/events.js
@@ -9,21 +9,18 @@ import {
 export function bindAddToDo() {
     dom.addToDo.addEventListener("click", () => {
         show(dom.toDoForm);
-        hide(dom.addToDo);
     });
 }
 
 export function bindAddProject() {
     dom.addProject.addEventListener("click", () => {
         show(dom.projectForm);
-        hide(dom.addProject);
     });
 }
 
 export function bindSubmitToDoForm(callback) {
     dom.toDoForm.addEventListener("submit", () => {
         hide(dom.toDoForm);
-        show(dom.addToDo);
         const f = dom.toDoForm.elements;
         callback(f.title.value, f.description.value,
             f.dueDate.value, f.priority.value,
@@ -34,7 +31,6 @@ export function bindSubmitToDoForm(callback) {
 export function bindSubmitProjectForm(callback) {
     dom.projectForm.addEventListener("submit", () => {
         hide(dom.projectForm);
-        show(dom.addProject);
         const f = dom.projectForm.elements;
         callback(f.title.value);
     });
